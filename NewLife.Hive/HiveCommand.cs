ec40a3ff5497e72b76b6497c27eab88d1cfa0e85
大石头 <nnhy@vip.qq.com>
2025-07-13 10:55:16 +0800

    最新Dispose

--- NewLife.Hive/HiveCommand.cs
+++ NewLife.Hive/HiveCommand.cs
@@ -1,184 +1,180 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections;
 using NewLife.Data;
 using NewLife.Hive2;
 
-namespace NewLife.Hive
+namespace NewLife.Hive;
+
+/// <summary>命令</summary>
+public class HiveCommand : DisposeBase
 {
-    /// <summary>命令</summary>
-    public class HiveCommand : DisposeBase
+    #region 属性
+    private TSessionHandle _Session;
+    private TCLIService.Client _Client;
+    private TOperationHandle _Operation;
+    private TTableSchema _LastSchema;
+
+    public TProtocolVersion Version { get; }
+    #endregion
+
+    #region 构造
+    public HiveCommand(TSessionHandle session, TCLIService.Client client, TProtocolVersion version = TProtocolVersion.V7)
     {
-        #region 属性
-        private TSessionHandle _Session;
-        private TCLIService.Client _Client;
-        private TOperationHandle _Operation;
-        private TTableSchema _LastSchema;
-
-        public TProtocolVersion Version { get; }
-        #endregion
-
-        #region 构造
-        public HiveCommand(TSessionHandle session, TCLIService.Client client, TProtocolVersion version = TProtocolVersion.V7)
-        {
-            _Session = session;
-            _Client = client;
-            Version = version;
-        }
-
-        protected override void OnDispose(Boolean disposing)
-        {
-            base.OnDispose(disposing);
-
-            CloseOperation();
-
-            _LastSchema = null;
-            _Client = null;
-            _Session = null;
-        }
-        #endregion
-
-        #region 执行
-        public void Execute(String statement)
-        {
-            CloseOperation();
-
-            var req = new TExecuteStatementReq()
-            {
-                SessionHandle = _Session,
-                Statement = statement,
-            };
-
-            _LastSchema = null;
-
-            var resp = _Client.ExecuteStatement(req);
-            resp.Status.CheckStatus();
-            _Operation = resp.OperationHandle;
-        }
-
-        private void CloseOperation()
-        {
-            if (_Operation != null)
-            {
-                var req = new TCloseOperationReq
-                {
-                    OperationHandle = _Operation
-                };
-                var resp = _Client.CloseOperation(req);
-                resp.Status.CheckStatus();
-            }
-        }
-        #endregion
-
-        #region 查询
-        /// <summary>批量获取数据</summary>
-        /// <param name="size"></param>
-        /// <returns></returns>
-        public DbTable Fetch(Int32 size)
-        {
-            var cs = GetSchema()?.Columns;
-            if (cs == null || cs.Count == 0) return null;
-
-            // V5根据行获取数据，不支持
-            if (Version <= TProtocolVersion.V5) throw new NotSupportedException();
-
-            // 获取列信息
-            var dt = new DbTable
-            {
-                Columns = cs.Select(i => i.ColumnName).ToArray()
-            };
-
-            //  获取数据
-            var set = FetchData(size);
-            if (set?.Columns != null && set.Columns.Count > 0)
-            {
-                var cols = new List<IList>();
-                foreach (var item in set.Columns)
-                {
-                    cols.Add(GetrValue(item));
-                }
-
-                var rowCount = cols[0].Count;
-                dt.Rows = new List<Object[]>(rowCount);
-                if (rowCount > 0)
-                {
-                    // 修补类型
-                    if (dt.Types == null || dt.Types.Length != cols.Count)
-                    {
-                        dt.Types = new Type[cols.Count];
-                        for (var k = 0; k < cols.Count; k++)
-                        {
-                            dt.Types[k] = cols[k][0]?.GetType();
-                        }
-                    }
-
-                    // 遍历所有行
-                    for (var i = 0; i < rowCount; i++)
-                    {
-                        var row = new Object[cols.Count];
-                        for (var k = 0; k < cols.Count; k++)
-                        {
-                            row[k] = cols[k][i];
-                        }
-                        dt.Rows.Add(row);
-                    }
-                }
-            }
-
-            return dt;
-        }
-
-        private IList GetrValue(TColumn value)
-        {
-            if (value.__isset.stringVal)
-                return value.StringVal.Values;
-            else if (value.__isset.i32Val)
-                return value.I32Val.Values;
-            else if (value.__isset.boolVal)
-                return value.BoolVal.Values;
-            else if (value.__isset.doubleVal)
-                return value.DoubleVal.Values;
-            else if (value.__isset.byteVal)
-                return value.ByteVal.Values;
-            else if (value.__isset.i64Val)
-                return value.I64Val.Values;
-            else if (value.__isset.i16Val)
-                return value.I16Val.Values;
-            else
-                return null;
-        }
-
-        public TRowSet FetchData(Int32 count = Int32.MaxValue)
-        {
-            if (_Operation == null || !_Operation.HasResultSet) return null;
-
-            var req = new TFetchResultsReq()
-            {
-                MaxRows = count,
-                Orientation = TFetchOrientation.FETCH_NEXT,
-                OperationHandle = _Operation,
-            };
-            var rs = _Client.FetchResults(req);
-            rs.Status.CheckStatus();
-
-            return rs.Results;
-        }
-
-        public TTableSchema GetSchema()
-        {
-            if (_Operation == null || !_Operation.HasResultSet) return null;
-
-            if (_LastSchema == null)
-            {
-                var req = new TGetResultSetMetadataReq(_Operation);
-                var rs = _Client.GetResultSetMetadata(req);
-                rs.Status.CheckStatus();
-                _LastSchema = rs.Schema;
-            }
-
-            return _LastSchema;
-        }
-        #endregion
+        _Session = session;
+        _Client = client;
+        Version = version;
     }
+
+    protected override void Dispose(Boolean disposing)
+    {
+        base.Dispose(disposing);
+
+        CloseOperation();
+
+        _LastSchema = null;
+        _Client = null;
+        _Session = null;
+    }
+    #endregion
+
+    #region 执行
+    public void Execute(String statement)
+    {
+        CloseOperation();
+
+        var req = new TExecuteStatementReq()
+        {
+            SessionHandle = _Session,
+            Statement = statement,
+        };
+
+        _LastSchema = null;
+
+        var resp = _Client.ExecuteStatement(req);
+        resp.Status.CheckStatus();
+        _Operation = resp.OperationHandle;
+    }
+
+    private void CloseOperation()
+    {
+        if (_Operation != null)
+        {
+            var req = new TCloseOperationReq
+            {
+                OperationHandle = _Operation
+            };
+            var resp = _Client.CloseOperation(req);
+            resp.Status.CheckStatus();
+        }
+    }
+    #endregion
+
+    #region 查询
+    /// <summary>批量获取数据</summary>
+    /// <param name="size"></param>
+    /// <returns></returns>
+    public DbTable Fetch(Int32 size)
+    {
+        var cs = GetSchema()?.Columns;
+        if (cs == null || cs.Count == 0) return null;
+
+        // V5根据行获取数据，不支持
+        if (Version <= TProtocolVersion.V5) throw new NotSupportedException();
+
+        // 获取列信息
+        var dt = new DbTable
+        {
+            Columns = cs.Select(i => i.ColumnName).ToArray()
+        };
+
+        //  获取数据
+        var set = FetchData(size);
+        if (set?.Columns != null && set.Columns.Count > 0)
+        {
+            var cols = new List<IList>();
+            foreach (var item in set.Columns)
+            {
+                cols.Add(GetrValue(item));
+            }
+
+            var rowCount = cols[0].Count;
+            dt.Rows = new List<Object[]>(rowCount);
+            if (rowCount > 0)
+            {
+                // 修补类型
+                if (dt.Types == null || dt.Types.Length != cols.Count)
+                {
+                    dt.Types = new Type[cols.Count];
+                    for (var k = 0; k < cols.Count; k++)
+                    {
+                        dt.Types[k] = cols[k][0]?.GetType();
+                    }
+                }
+
+                // 遍历所有行
+                for (var i = 0; i < rowCount; i++)
+                {
+                    var row = new Object[cols.Count];
+                    for (var k = 0; k < cols.Count; k++)
+                    {
+                        row[k] = cols[k][i];
+                    }
+                    dt.Rows.Add(row);
+                }
+            }
+        }
+
+        return dt;
+    }
+
+    private IList GetrValue(TColumn value)
+    {
+        if (value.__isset.stringVal)
+            return value.StringVal.Values;
+        else if (value.__isset.i32Val)
+            return value.I32Val.Values;
+        else if (value.__isset.boolVal)
+            return value.BoolVal.Values;
+        else if (value.__isset.doubleVal)
+            return value.DoubleVal.Values;
+        else if (value.__isset.byteVal)
+            return value.ByteVal.Values;
+        else if (value.__isset.i64Val)
+            return value.I64Val.Values;
+        else if (value.__isset.i16Val)
+            return value.I16Val.Values;
+        else
+            return null;
+    }
+
+    public TRowSet FetchData(Int32 count = Int32.MaxValue)
+    {
+        if (_Operation == null || !_Operation.HasResultSet) return null;
+
+        var req = new TFetchResultsReq()
+        {
+            MaxRows = count,
+            Orientation = TFetchOrientation.FETCH_NEXT,
+            OperationHandle = _Operation,
+        };
+        var rs = _Client.FetchResults(req);
+        rs.Status.CheckStatus();
+
+        return rs.Results;
+    }
+
+    public TTableSchema GetSchema()
+    {
+        if (_Operation == null || !_Operation.HasResultSet) return null;
+
+        if (_LastSchema == null)
+        {
+            var req = new TGetResultSetMetadataReq(_Operation);
+            var rs = _Client.GetResultSetMetadata(req);
+            rs.Status.CheckStatus();
+            _LastSchema = rs.Schema;
+        }
+
+        return _LastSchema;
+    }
+    #endregion
 }
--- NewLife.Hive/HiveConnection.cs
+++ NewLife.Hive/HiveConnection.cs
@@ -1,95 +1,98 @@
-﻿using System;
-using NewLife.Hive2;
+﻿using NewLife.Hive2;
+using NewLife.Log;
 using NewLife.Thrift.Protocol;
 using NewLife.Thrift.Transport;
 
-namespace NewLife.Hive
+namespace NewLife.Hive;
+
+public static class Hive2Helper
 {
-    public static class Hive2Helper
+    public static void CheckStatus(this TStatus status)
     {
-        public static void CheckStatus(this TStatus status)
+        if (status.__isset.errorMessage)
         {
-            if (status.__isset.errorMessage)
-            {
-                throw new Exception(status.ErrorMessage);
-            }
+            throw new Exception(status.ErrorMessage);
         }
     }
+}
 
-    /// <summary>Hive连接</summary>
-    public class HiveConnection : DisposeBase
+/// <summary>Hive连接</summary>
+public class HiveConnection : DisposeBase
+{
+    #region 属性
+    private TSaslClientTransport _Transport;
+    private TCLIService.Client _Client;
+    private TSessionHandle _Session;
+
+    public TProtocolVersion Version { get; }
+    #endregion
+
+    #region 构造
+    public HiveConnection(String host, Int32 port, String userName = "None", String password = "None", TProtocolVersion version = TProtocolVersion.V7)
     {
-        #region 属性
-        private TSaslClientTransport _Transport;
-        private TCLIService.Client _Client;
-        private TSessionHandle _Session;
-
-        public TProtocolVersion Version { get; }
-        #endregion
-
-        #region 构造
-        public HiveConnection(String host, Int32 port, String userName = "None", String password = "None", TProtocolVersion version = TProtocolVersion.V7)
-        {
-            var socket = new TSocket(host, port);
-            _Transport = new TSaslClientTransport(socket, userName, password);
-            var protocol = new TBinaryProtocol(_Transport);
-            _Client = new TCLIService.Client(protocol);
-            Version = version;
-        }
-
-        protected override void OnDispose(Boolean disposing)
-        {
-            base.OnDispose(disposing);
-
-            try
-            {
-                Close();
-            }
-            catch { }
-
-            _Client = null;
-            _Transport = null;
-            _Session = null;
-        }
-        #endregion
-
-        #region 方法
-        public void Open()
-        {
-            if (_Transport != null && !_Transport.IsOpen) _Transport.Open();
-            if (_Session == null) _Session = GetSession();
-        }
-
-        public void Close()
-        {
-            if (_Session != null) CloseSession();
-            if (_Transport != null && _Transport.IsOpen) _Transport.Close();
-        }
-
-        private TSessionHandle GetSession()
-        {
-            var req = new TOpenSessionReq(Version);
-            var resp = _Client.OpenSession(req);
-            resp.Status.CheckStatus();
-            return resp.SessionHandle;
-        }
-
-        private void CloseSession()
-        {
-            var req = new TCloseSessionReq
-            {
-                SessionHandle = _Session
-            };
-            var resp = _Client.CloseSession(req);
-            resp.Status.CheckStatus();
-        }
-
-        public HiveCommand CreateCommand()
-        {
-            Open();
-
-            return new HiveCommand(_Session, _Client, Version);
-        }
-        #endregion
+        var socket = new TSocket(host, port);
+        _Transport = new TSaslClientTransport(socket, userName, password);
+        var protocol = new TBinaryProtocol(_Transport);
+        _Client = new TCLIService.Client(protocol);
+        Version = version;
     }
+
+    protected override void Dispose(Boolean disposing)
+    {
+        base.Dispose(disposing);
+
+        try
+        {
+            Close();
+        }
+        catch (Exception ex)
+        {
+
+            XTrace.WriteLine($"Error while closing: {ex.Message}");
+        }
+
+        _Client = null;
+        _Transport = null;
+        _Session = null;
+    }
+    #endregion
+
+    #region 方法
+    public void Open()
+    {
+        if (_Transport != null && !_Transport.IsOpen) _Transport.Open();
+        if (_Session == null) _Session = GetSession();
+    }
+
+    public void Close()
+    {
+        if (_Session != null) CloseSession();
+        if (_Transport != null && _Transport.IsOpen) _Transport.Close();
+    }
+
+    private TSessionHandle GetSession()
+    {
+        var req = new TOpenSessionReq(Version);
+        var resp = _Client.OpenSession(req);
+        resp.Status.CheckStatus();
+        return resp.SessionHandle;
+    }
+
+    private void CloseSession()
+    {
+        var req = new TCloseSessionReq
+        {
+            SessionHandle = _Session
+        };
+        var resp = _Client.CloseSession(req);
+        resp.Status.CheckStatus();
+    }
+
+    public HiveCommand CreateCommand()
+    {
+        Open();
+
+        return new HiveCommand(_Session, _Client, Version);
+    }
+    #endregion
 }
